79f94e1dc9bcd543fa056ff71a6850f6d52aad92
leay21 <yaelt143@gmail.com>
2025-06-05 06:53:26 -0600

    Primer commit

---
+++ phrases/src/main/java/com/ejemplo/phrases/Phrase.java
@@ -0,0 +1,15 @@
+package com.ejemplo.phrases;
+import java.time.LocalDateTime;
+
+public class Phrase {
+    private Long id;
+    private String originalText;
+    private String translatedText;
+    private LocalDateTime createdAt;
+
+    public Phrase() {
+
+    }
+
+    
+}
---
+++ phrases/src/test/java/com/ejemplo/phrases/PhrasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.phrases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhrasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
